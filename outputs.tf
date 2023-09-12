4492a2f5954b4e359a55cf61254bda41ebb8c4f5
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2023-09-12 16:45:12 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,10 @@ output "sql_server_s3_backup_bucket_arn" {
   value       = join("", module.sql_server_s3_backup.*.bucket_arn)
   description = "S3 bucket ARN for SQL Server native backups and transaction logs for point-in-time recovery"
 }
-
+output "sql_server_s3_audit_logs_bucket_arn" {
+  value       = join("", module.sql_server_s3_audit_logs.*.bucket_arn)
+  description = "S3 bucket ARN for SQL Server Audit logs"
+}
 output "cmk_key_id" {
   value       = aws_kms_key.cmk.key_id
   description = "KMS key ID of CMK created for RDS storage encryption"
