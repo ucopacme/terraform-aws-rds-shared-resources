e23c33b57438456860463173db0e6dcb15bebf46
George Holbert <gholbert@ucop.edu>
2024-02-17 15:35:45 -0800

    typo fix for definition of local create_sql_server_s3_audit_logs_bucket

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   create_sql_server_s3_backup_bucket     = var.sql_server_s3_backup_bucket_name != "" ? true : false
-  create_sql_server_s3_audit_logs_bucket = var.sql_server_s3_backup_bucket_name != "" ? true : false
+  create_sql_server_s3_audit_logs_bucket = var.sql_server_s3_audit_logs_bucket_name != "" ? true : false
   create_sql_server_s3_role              = local.create_sql_server_s3_backup_bucket
   create_sql_server_s3_policy            = local.create_sql_server_s3_backup_bucket
 }
